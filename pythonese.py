d47b487ffecb0f0eb1fa61a2b1da96cc67d77528
Videsh Arivazhagan <63670445+arivvid27@users.noreply.github.com>
2025-03-26 18:15:23 +0000

    minor eerror patch

--- pythonese.py
+++ pythonese.py
@@ -16,13 +16,13 @@ The Main code for the Pythonese application
 # ! The code below is written by me, Videsh Arivazhagan, the author of this project.
 
 import subprocess
+import sys
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', './requirements.txt'])
 
 import os
 import time
 import asyncio
 import threading
-import colorama
 import keyboard
 import speech_recognition as sr
 from gtts import gTTS
