b641493e85b624ffd6719a1fe93a3b551b28c96f
Videsh Arivazhagan <arivvid27@gmail.com>
2025-03-29 14:57:00 -0700

    Files added. Not yet tested. Many problems with PEP8

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', 'sqlite:///pythonese.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY', 'AIzaSyCfb1AlLLYl9V3gEODD1JKwsuLTqQi0E3Q')
+    
+    SUPPORTED_LANGUAGES = {
+        'en': 'English',
+        'es': 'Spanish',
+        'fr': 'French',
+        'de': 'German',
+        'it': 'Italian',
+        'pt': 'Portuguese',
+        'ru': 'Russian',
+        'ja': 'Japanese',
+        'ko': 'Korean',
+        'zh': 'Chinese',
+        'ar': 'Arabic',
+        'hi': 'Hindi'
+    }
+    
---
+++ services/__init__.py
@@ -0,0 +1,7 @@
+from .translation import TranslationService
+from .stt import SpeechToTextService
+from .tts import TextToSpeechService
+
+translation_service = TranslationService()
+stt_service = SpeechToTextService()
+tts_service = TextToSpeechService()
